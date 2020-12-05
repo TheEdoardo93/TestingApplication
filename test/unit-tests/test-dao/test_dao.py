14fa6515356a95186019892b5f2b8dae6fe24a24
edoardo.casiraghi <edoardo.casiraghi.93@gmail.com>
2020-12-05 18:01:00 +0100

    Updated requirements.txt and added test_dao.py script

---
+++ test/unit-tests/test-dao/test_dao.py
@@ -0,0 +1,23 @@
+import pytest
+import sqlite3
+
+class TestDAO(object):
+    @pytest.fixture(scope='module')
+    def init_db(self, tmpdir):
+        pass
+
+
+    @pytest.fixture(scope='module')
+    def create_connection_cursor_to_db(self):
+        ### SET-UP ###
+        # Define a connection to SQLite Database to the "users" table
+        self.connection = sqlite3.connect('users.db')
+        # cursor object
+        self.cursor = self.connection.cursor()
+
+        yield
+
+        ### TEAR-DOWN ###
+        # Close the cursor and the connection to the SQLite database
+        self.cursor.close()
+        self.connection.close()
