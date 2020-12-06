7305c35f825ae8f9ebc1135a3cfdfd1c0e114307
edoardo.casiraghi <edoardo.casiraghi.93@gmail.com>
2020-12-06 18:02:47 +0100

    Added tox.ini and setup.py files in order to use Tox correctly

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='src',
+   version='0.0.1',
+   description='TestingApplication description',
+   author='Edoardo Casiraghi',
+   author_email='edoardo.casiraghi.93@gmail.com',
+   packages=['src']
+)
